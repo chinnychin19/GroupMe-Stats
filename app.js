a8d77573c70d82c427591f13f1c2c1fd9852c8d4
Chinmay Patwardhan <chinnychin19@gmail.com>
2015-05-17 17:30:13 -0400

    added group name, description, and update time to the stats page

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require('express')
-  , stylus = require('stylus')
-  , nib = require('nib')
-  , session = require('express-session')
-  , groupme = require('./my_modules/groupme.js')
-  , mongo = require('./my_modules/mongo.js')
-  , sorter = require('./my_modules/sortMembers.js');
+var express = require('express'),
+  stylus = require('stylus'),
+  nib = require('nib'),
+  session = require('express-session'),
+  groupme = require('./my_modules/groupme.js'),
+  mongo = require('./my_modules/mongo.js'),
+  sorter = require('./my_modules/sortMembers.js');
 
 var app = express()
 
@@ -17,11 +17,10 @@ function compileStylus(str, path) {
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 
-app.use(stylus.middleware(
-  { src: __dirname + '/public'
-  , compile: compileStylus
-  }
-))
+app.use(stylus.middleware({
+  src: __dirname + '/public',
+  compile: compileStylus
+}))
 
 app.use(express.static(__dirname + '/public'))
 
@@ -29,8 +28,7 @@ app.use(session({
   secret: process.env.GMSTATS_SESSION_SECRET,
   saveUninitialized: true,
   resave: true
-  })
-);
+}));
 
 
 /*
@@ -38,13 +36,13 @@ app.use(session({
 */
 
 // pre-login
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   // redirect to groupme login
-  res.redirect("https://api.groupme.com/oauth/authorize?client_id="+process.env.GMSTATS_CLIENT_ID);
+  res.redirect("https://api.groupme.com/oauth/authorize?client_id=" + process.env.GMSTATS_CLIENT_ID);
 });
 
 // post-login (loggedin)
-app.get('/logged-in', function (req, res) {
+app.get('/logged-in', function(req, res) {
   // TODO:
   // 1) store access token
   // 2) Get user info
@@ -54,42 +52,48 @@ app.get('/logged-in', function (req, res) {
   console.log(req.query.access_token)
   groupme.getUserInfo(req.session, function(userInfo) {
     req.session.userInfo = userInfo;
-    res.redirect('/groups');    
+    res.redirect('/groups');
   });
 });
 
 // groups
-app.get('/groups', function (req, res) {
+app.get('/groups', function(req, res) {
   // TODO: list all groups with number of members and image in a grid layout
   groupme.getAllGroups(req.session, function(groups) {
-    var object = { "groups" : groups};
+    var object = {
+      "groups": groups
+    };
     res.render('all_groups', object);
   });
 });
 
 // specific group's stats
-app.get('/groups/stats/:group_id', function (req, res) {
+app.get('/groups/stats/:group_id', function(req, res) {
   // TODO: present a page with sortable stats
   var session = req.session;
   var group_id = req.params.group_id;
-  mongo.isGroupInDB(session, group_id, 
+  mongo.isGroupInDB(session, group_id,
     function() { // if group is found in DB
-      mongo.isGroupProcessing(session, group_id, 
+      mongo.isGroupProcessing(session, group_id,
         function() { // if group is processing
-          mongo.getStats(session, group_id, function(stats) {
+          mongo.getGroupData(session, group_id, function(data) {
             res.send("Still processing... Try refreshing the page in a few moments...");
             // TODO: render jade file with processing true
           });
-        }, 
+        },
         function() { // if group is done processing
-          mongo.getStats(session, group_id, function(stats) {
-            console.log(JSON.stringify(stats));
+          mongo.getGroupData(session, group_id, function(data) {
+            console.log(JSON.stringify(data));
             var sortField = req.query.q ? req.query.q : "ratio"; // sort by ratio by default
-            console.log("sorting by "+sortField);
-            sorter.sortBy(stats, sortField);
-            var object = { 
-              "membersList" : stats, 
-              "group_id" : group_id 
+            console.log("sorting by " + sortField);
+            sorter.sortBy(data.stats, sortField);
+            var object = {
+              "membersList": data.stats,
+              "group_id": group_id,
+              "group_name": data.name,
+              "group_description": data.description,
+              "group_image_url": data.image_url,
+              "last_updated": data.last_scraped
             };
             res.render('stats', object); // TODO: include that processing is complete
           });
@@ -98,13 +102,13 @@ app.get('/groups/stats/:group_id', function (req, res) {
     function() { // if group is NOT found in DB
       mongo.addGroupToDB(session, group_id, function() {
         // re-enter this logic gate with the group added to the DB
-        res.redirect('/groups/stats/'+group_id);
+        res.redirect('/groups/stats/' + group_id);
       });
     });
 });
 
 // specific group's top messages
-app.get('/groups/top-messages/:group_id', function (req, res) {
+app.get('/groups/top-messages/:group_id', function(req, res) {
   // TODO: present a page with top messages, in order
   // Messages should include sender name, timestamp, number of likes, and photo
   res.send("TODO: implement");
--- my_modules/mongo.js
+++ my_modules/mongo.js
@@ -1,62 +1,87 @@
-var MongoClient = require('mongodb').MongoClient
-  , groupme = require('./groupme.js')
-  , unirest = require('unirest');
+var MongoClient = require('mongodb').MongoClient,
+  groupme = require('./groupme.js'),
+  unirest = require('unirest');
 
 var MONGO_CONNECT_URI = process.env.GMSTATS_MONGO_CONNECT_URI;
 var MONGO_COLLECTION_NAME = "Groups";
 
 
 exports.isGroupInDB = function isGroupInDB(session, group_id, callback_true, callback_false) {
-  MongoClient.connect(MONGO_CONNECT_URI, function (err, db) {
+  MongoClient.connect(MONGO_CONNECT_URI, function(err, db) {
     if (err) {
       console.log(err);
       return;
     }
     var collection = db.collection(MONGO_COLLECTION_NAME);
-    collection.find({"id" : group_id}).toArray(function(err, items) {
+    collection.find({
+      "id": group_id
+    }).toArray(function(err, items) {
       if (items.length == 1) {
         callback_true();
       } else if (items.length == 0) {
         callback_false();
       } else {
-        throw new Error("Error! More than one instance of same group in DB! Group id: "+group_id);
+        throw new Error("Error! More than one instance of same group in DB! Group id: " + group_id);
       }
     });
   });
 }
 
 exports.isGroupProcessing = function isGroupProcessing(session, group_id, callback_true, callback_false) {
-  MongoClient.connect(MONGO_CONNECT_URI, function (err, db) {
+  MongoClient.connect(MONGO_CONNECT_URI, function(err, db) {
     if (err) {
       console.log(err);
       return;
     }
     var collection = db.collection(MONGO_COLLECTION_NAME);
-    collection.find({"id" : group_id, "is_processing":true}).toArray(function(err, items) {
+    collection.find({
+      "id": group_id,
+      "is_processing": true
+    }).toArray(function(err, items) {
       if (items.length == 1) {
         callback_true();
       } else if (items.length == 0) {
         callback_false();
       } else {
-        throw new Error("Error! More than one instance of same group in DB! Group id: "+group_id);
+        throw new Error("Error! More than one instance of same group in DB! Group id: " + group_id);
       }
     });
   });
 }
 
-exports.getStats = function getStats(session, group_id, callback) { // callback(stats)
-  MongoClient.connect(MONGO_CONNECT_URI, function (err, db) {
+// exports.getStats = function getStats(session, group_id, callback) { // callback(stats)
+//   MongoClient.connect(MONGO_CONNECT_URI, function (err, db) {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     var collection = db.collection(MONGO_COLLECTION_NAME);
+//     collection.find({"id" : group_id}).toArray(function(err, items) {
+//       if (items.length == 1) {
+//         var stats = items[0].stats;
+//         callback(stats);
+//       } else {
+//         throw new Error("Error! Expected one entry for group "+group_id+". Found "+items.length+" entries.");
+//       }
+//     });
+//   });
+// }
+
+exports.getGroupData = function getGroupData(session, group_id, callback) {
+  MongoClient.connect(MONGO_CONNECT_URI, function(err, db) {
     if (err) {
       console.log(err);
       return;
     }
     var collection = db.collection(MONGO_COLLECTION_NAME);
-    collection.find({"id" : group_id}).toArray(function(err, items) {
+    collection.find({
+      "id": group_id
+    }).toArray(function(err, items) {
       if (items.length == 1) {
-        var stats = items[0].stats;
-        callback(stats);
+        var data = items[0];
+        callback(data);
       } else {
-        throw new Error("Error! Expected one entry for group "+group_id+". Found "+items.length+" entries.");
+        throw new Error("Error! Expected one entry for group " + group_id + ". Found " + items.length + " entries.");
       }
     });
   });
@@ -66,8 +91,8 @@ exports.addGroupToDB = function addGroupToDB(session, group_id, callback) {
   groupme.getGroupInfo(session, group_id, function(group) { // get the group info
     group.is_processing = true; // mark the group as processing
     group.stats = { // initialize stats as empty
-      "member_stats" : [],
-      "top_messages" : []
+      "member_stats": [],
+      "top_messages": []
     };
 
     MongoClient.connect(MONGO_CONNECT_URI, function(err, db) {
@@ -76,7 +101,9 @@ exports.addGroupToDB = function addGroupToDB(session, group_id, callback) {
         return;
       }
       var collection = db.collection(MONGO_COLLECTION_NAME);
-      collection.insert(group, {"safe":true}, function(err, object) { // add the group to the DB
+      collection.insert(group, {
+        "safe": true
+      }, function(err, object) { // add the group to the DB
         callback();
         launchScrape(session, group_id); // launch the scrape. when complete, it will mark processing as false
       });
@@ -86,9 +113,9 @@ exports.addGroupToDB = function addGroupToDB(session, group_id, callback) {
 
 // Assumption is that this group already exists in the DB
 function launchScrape(session, group_id) {
-  var a = unirest.get("http://gmstats-scraper.herokuapp.com/groups/scrape/"+group_id)
-  .query("access_token="+session.access_token)
-  .end(function(res) {
-    console.log("successfully scraped group "+group_id);
-  });
+  var a = unirest.get("http://gmstats-scraper.herokuapp.com/groups/scrape/" + group_id)
+    .query("access_token=" + session.access_token)
+    .end(function(res) {
+      console.log("successfully scraped group " + group_id);
+    });
 }
