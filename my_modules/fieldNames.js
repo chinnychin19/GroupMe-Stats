f31a622e487f0f7ab737fd5a846df22a09715864
Chinmay Patwardhan <chinnychin19@gmail.com>
2018-07-02 23:38:56 -0700

    minor clean ups

--- my_modules/fieldNames.js
+++ my_modules/fieldNames.js
@@ -5,4 +5,11 @@ exports.LIKES_GIVEN = "likesGiven";
 exports.SELF_LIKES = "selfLikes";
 exports.NAME = "name";
 
-exports.ALL_FIELDS = [exports.RATIO, exports.MESSAGE_COUNT, exports.LIKES_RECEIVED, exports.LIKES_GIVEN, exports.SELF_LIKES, exports.NAME];
+exports.ALL_FIELDS = [
+  exports.RATIO,
+  exports.MESSAGE_COUNT,
+  exports.LIKES_RECEIVED,
+  exports.LIKES_GIVEN,
+  exports.SELF_LIKES,
+  exports.NAME
+];
