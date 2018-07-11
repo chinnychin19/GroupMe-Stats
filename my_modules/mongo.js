c48391f50e41cd63e63f94958ab7b61f1684322a
Chinmay Patwardhan <chinnychin19@gmail.com>
2018-07-10 23:27:19 -0700

    add mongo client method to update an existing group

--- my_modules/mongo.js
+++ my_modules/mongo.js
@@ -1,52 +1,52 @@
 var MongoClient = require('mongodb').MongoClient,
-  groupme = require('./groupme.js'),
-  unirest = require('unirest');
+    groupme = require('./groupme.js'),
+    unirest = require('unirest');
 
 var MONGO_CONNECT_URI = process.env.GMSTATS_MONGO_CONNECT_URI;
 var MONGO_COLLECTION_NAME = "Groups";
 
 
 exports.isGroupInDB = function isGroupInDB(session, group_id, callback_true, callback_false) {
-  MongoClient.connect(MONGO_CONNECT_URI, function(err, db) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    var collection = db.collection(MONGO_COLLECTION_NAME);
-    collection.find({
-      "id": group_id
-    }).toArray(function(err, items) {
-      if (items.length == 1) {
-        callback_true();
-      } else if (items.length == 0) {
-        callback_false();
-      } else {
-        throw new Error("Error! More than one instance of same group in DB! Group id: " + group_id);
-      }
+    MongoClient.connect(MONGO_CONNECT_URI, function(err, db) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        var collection = db.collection(MONGO_COLLECTION_NAME);
+        collection.find({
+            "id": group_id
+        }).toArray(function(err, items) {
+            if (items.length == 1) {
+                callback_true();
+            } else if (items.length == 0) {
+                callback_false();
+            } else {
+                throw new Error("Error! More than one instance of same group in DB! Group id: " + group_id);
+            }
+        });
     });
-  });
 }
 
 exports.isGroupProcessing = function isGroupProcessing(session, group_id, callback_true, callback_false) {
-  MongoClient.connect(MONGO_CONNECT_URI, function(err, db) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    var collection = db.collection(MONGO_COLLECTION_NAME);
-    collection.find({
-      "id": group_id,
-      "is_processing": true
-    }).toArray(function(err, items) {
-      if (items.length == 1) {
-        callback_true();
-      } else if (items.length == 0) {
-        callback_false();
-      } else {
-        throw new Error("Error! More than one instance of same group in DB! Group id: " + group_id);
-      }
+    MongoClient.connect(MONGO_CONNECT_URI, function(err, db) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        var collection = db.collection(MONGO_COLLECTION_NAME);
+        collection.find({
+            "id": group_id,
+            "is_processing": true
+        }).toArray(function(err, items) {
+            if (items.length == 1) {
+                callback_true();
+            } else if (items.length == 0) {
+                callback_false();
+            } else {
+                throw new Error("Error! More than one instance of same group in DB! Group id: " + group_id);
+            }
+        });
     });
-  });
 }
 
 // exports.getStats = function getStats(session, group_id, callback) { // callback(stats)
@@ -68,54 +68,74 @@ exports.isGroupProcessing = function isGroupProcessing(session, group_id, callba
 // }
 
 exports.getGroupData = function getGroupData(session, group_id, callback) {
-  MongoClient.connect(MONGO_CONNECT_URI, function(err, db) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    var collection = db.collection(MONGO_COLLECTION_NAME);
-    collection.find({
-      "id": group_id
-    }).toArray(function(err, items) {
-      if (items.length == 1) {
-        var data = items[0];
-        callback(data);
-      } else {
-        throw new Error("Error! Expected one entry for group " + group_id + ". Found " + items.length + " entries.");
-      }
+    MongoClient.connect(MONGO_CONNECT_URI, function(err, db) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        var collection = db.collection(MONGO_COLLECTION_NAME);
+        collection.find({
+            "id": group_id
+        }).toArray(function(err, items) {
+            if (items.length == 1) {
+                var data = items[0];
+                callback(data);
+            } else {
+                throw new Error("Error! Expected one entry for group " + group_id + ". Found " + items.length + " entries.");
+            }
+        });
     });
-  });
 }
 
 exports.addGroupToDB = function addGroupToDB(session, group_id, callback) {
-  groupme.getGroupInfo(session, group_id, function(group) { // get the group info
-    group.is_processing = true; // mark the group as processing
-    group.stats = { // initialize stats as empty
-      "member_stats": [],
-      "top_messages": []
-    };
+    groupme.getGroupInfo(session, group_id, function(group) { // get the group info
+        group.is_processing = true; // mark the group as processing
+        group.stats = { // initialize stats as empty
+            "member_stats": [],
+            "top_messages": []
+        };
 
-    MongoClient.connect(MONGO_CONNECT_URI, function(err, db) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      var collection = db.collection(MONGO_COLLECTION_NAME);
-      collection.insert(group, {
-        "safe": true
-      }, function(err, object) { // add the group to the DB
-        callback();
-        launchScrape(session, group_id); // launch the scrape. when complete, it will mark processing as false
-      });
+        MongoClient.connect(MONGO_CONNECT_URI, function(err, db) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            var collection = db.collection(MONGO_COLLECTION_NAME);
+            collection.insert(group, {
+                "safe": true
+            }, function(err, object) { // add the group to the DB
+                callback();
+                launchScrape(session, group_id); // launch the scrape. when complete, it will mark processing as false
+            });
+        });
     });
-  });
+}
+
+// delete the group, then add it back
+exports.updateGroupInDB = function updateGroupInDB(session, group_id, callback) {
+    MongoClient.connect(MONGO_CONNECT_URI, function(err, db) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        var collection = db.collection(MONGO_COLLECTION_NAME);
+        collection.deleteOne({
+            "group_id": group_id
+        }, function(err, object) { // add the group to the DB
+            if (err) {
+                console.log(err);
+                return;
+            }
+            addGroupToDB(session, group_id, callback);
+        });
+    });
 }
 
 // Assumption is that this group already exists in the DB
 function launchScrape(session, group_id) {
-  var a = unirest.get("http://gmstats-scraper.herokuapp.com/groups/scrape/" + group_id)
-    .query("access_token=" + session.access_token)
-    .end(function(res) {
-      console.log("successfully scraped group " + group_id);
-    });
+    var a = unirest.get("http://gmstats-scraper.herokuapp.com/groups/scrape/" + group_id)
+        .query("access_token=" + session.access_token)
+        .end(function(res) {
+            console.log("successfully scraped group " + group_id);
+        });
 }
